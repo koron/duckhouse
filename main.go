431a1039e99eaac42dfab7764d5573e3a4fa9704
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-02-26 23:50:30 +0900

    complete PoC

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/csv"
 	"fmt"
 	"io"
 	"log"
+	"math/rand/v2"
 	"net"
 	"net/http"
 	"strings"
@@ -14,87 +16,72 @@ import (
 	_ "github.com/duckdb/duckdb-go/v2"
 )
 
-func run() error {
-	db, err := sql.Open("duckdb", "")
-	if err != nil {
-		return err
-	}
-	defer db.Close()
+var (
+	idSet    sync.Map
+	connToID sync.Map
+	idToDB   sync.Map
+)
 
-	rows, err := db.Query(`SELECT 'hello', version(), 123 as NUM`)
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
-
+func duckhouseNewConnID(c net.Conn) uint64 {
 	for {
-		types, err := rows.ColumnTypes()
-		if err != nil {
-			return err
-		}
-		if len(types) > 0 {
-			fmt.Println("Column types:")
-			for i, typ := range types {
-				fmt.Printf("  #%d: name=%s %s\n", i, typ.Name(), typ.ScanType())
-			}
-		}
-		colNum := len(types)
-		values := make([]any, colNum)
-		for i := range colNum {
-			values[i] = new(any)
-		}
-		rowNum := 0
-		fmt.Println("Rows:")
-		for rows.Next() {
-			err := rows.Scan(values...)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("  #%d", rowNum)
-			for _, v := range values {
-				fmt.Printf(" %+v", *(v.(*any)))
-			}
-			fmt.Println()
-			rowNum++
-		}
-		if !rows.NextResultSet() {
-			break
+		id := rand.Uint64()
+		_, ok := idSet.LoadOrStore(id, true)
+		if !ok {
+			connToID.Store(c, id)
+			return id
 		}
 	}
-	return rows.Err()
 }
 
-var connToDB sync.Map
-
-func duckhouseBindNewDuckDB(c net.Conn) *sql.DB {
+func duckhouseGetDB(r *http.Request) (*sql.DB, uint64, error) {
+	id, ok := r.Context().Value(connIDKey{}).(uint64)
+	if !ok {
+		return nil, 0, fmt.Errorf("no connection ID assigned for request:%v", r)
+	}
+	rawdb, ok := idToDB.Load(id)
+	if ok {
+		return rawdb.(*sql.DB), 0, nil
+	}
 	db, err := sql.Open("duckdb", "")
 	if err != nil {
-		log.Printf("failed to open DuckDB: %s", err)
-		return nil
+		return nil, 0, err
 	}
 	db.SetMaxIdleConns(0)
-	connToDB.Store(c, db)
-	log.Printf("created sql.DB=%p for net.Conn=%p", db, c)
-	return db
+	idToDB.Store(id, db)
+	log.Printf("created sql.DB=%p for connID=%016x", db, id)
+	return db, id, nil
 }
 
-var dbKey = struct{}{}
+type connIDKey = struct{}
 
 func duckhouseConnContext(ctx context.Context, c net.Conn) context.Context {
-	db := duckhouseBindNewDuckDB(c)
-	return context.WithValue(ctx, dbKey, db)
+	id := duckhouseNewConnID(c)
+	return context.WithValue(ctx, connIDKey{}, id)
+}
+
+func duckhouseCloseConn(c net.Conn) error {
+	rawid, ok := connToID.LoadAndDelete(c)
+	if !ok {
+		return fmt.Errorf("no ID for net.Conn=%p", c)
+	}
+	id := rawid.(uint64)
+	idSet.Delete(id)
+	rawdb, ok := idToDB.LoadAndDelete(id)
+	if !ok {
+		return nil
+	}
+	db := rawdb.(*sql.DB)
+	db.Close()
+	log.Printf("closed sql.DB=%p for connID=%016x", db, id)
+	return nil
 }
 
 func duckhouseConnState(c net.Conn, s http.ConnState) {
 	if s == http.StateClosed {
-		v, ok := connToDB.LoadAndDelete(c)
-		if !ok {
-			log.Printf("no sql.DB for net.Conn=%p", c)
-			return
+		err := duckhouseCloseConn(c)
+		if err != nil {
+			log.Printf("failed to close conn: %s", err)
 		}
-		db := v.(*sql.DB)
-		db.Close()
-		log.Printf("closed sql.DB=%p for net.Conn=%p", db, c)
 	}
 }
 
@@ -116,17 +103,60 @@ func readQuery(r *http.Request) string {
 	return ""
 }
 
+func writeAsCSV(w http.ResponseWriter, rows *sql.Rows) error {
+	w.Header().Set("Content-Type", "text/csv")
+	w.WriteHeader(200)
+
+	ww := csv.NewWriter(w)
+
+	for {
+		// Write header
+		types, err := rows.ColumnTypes()
+		if err != nil {
+			return err
+		}
+		if len(types) > 0 {
+			names := make([]string, len(types))
+			fmt.Println("Column types:")
+			for i, typ := range types {
+				names[i] = typ.Name()
+				fmt.Printf("  #%d: name=%s %s\n", i, typ.Name(), typ.ScanType())
+			}
+			if err := ww.Write(names); err != nil {
+				return err
+			}
+		}
+		// Scan and write values (CSV body)
+		values := make([]any, len(types))
+		records := make([]string, len(types))
+		for i := range values {
+			values[i] = new(any)
+		}
+		for rows.Next() {
+			err := rows.Scan(values...)
+			if err != nil {
+				return err
+			}
+			for i, v := range values {
+				records[i] = fmt.Sprint(*(v.(*any)))
+			}
+			if err := ww.Write(records); err != nil {
+				return err
+			}
+		}
+		if !rows.NextResultSet() {
+			break
+		}
+	}
+
+	ww.Flush()
+	return nil
+}
+
 func duckhouseHandleQuery(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" && r.Method != "POST" {
 		w.WriteHeader(404)
 		io.WriteString(w, "Not Found\r\n")
-		return
-	}
-	db, ok := r.Context().Value(dbKey).(*sql.DB)
-	if !ok {
-		log.Printf("no DuckDB associated to request:%s", r)
-		w.WriteHeader(500)
-		io.WriteString(w, "No associated database, please contact admin\r\n")
 		return
 	}
 	query := readQuery(r)
@@ -136,7 +166,14 @@ func duckhouseHandleQuery(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	rows, err := db.Query(query)
+	db, id, err := duckhouseGetDB(r)
+	if err != nil {
+		w.WriteHeader(500)
+		io.WriteString(w, "No associated DB: "+err.Error())
+		return
+	}
+	log.Printf("query=%q connID=%016x", query, id)
+	rows, err := db.QueryContext(r.Context(), query)
 	if err != nil {
 		w.WriteHeader(500)
 		fmt.Fprintf(w, "Query failed: %s\r\n", err)
@@ -144,11 +181,11 @@ func duckhouseHandleQuery(w http.ResponseWriter, r *http.Request) {
 	}
 	defer rows.Close()
 
-	// TODO:
-	log.Printf("query=%s", query)
-	_ = db
-	w.WriteHeader(501)
-	io.WriteString(w, "Not Implemented Yet\r\n")
+	err = writeAsCSV(w, rows)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Serialization error: %s\r\n", err)
+	}
 }
 
 func duckhouseHandler(w http.ResponseWriter, r *http.Request) {
