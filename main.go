21866c1f2645c5ff85c2a08bda9f2cb9209360d0
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-03-03 12:32:34 +0900

    combined style access log to stdout
    Fix #2

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 	"math/rand/v2"
 	"net"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
+	"time"
 
 	_ "github.com/duckdb/duckdb-go/v2"
 )
@@ -20,6 +22,8 @@ var (
 	idSet    sync.Map
 	connToID sync.Map
 	idToDB   sync.Map
+
+	accessLogWriter io.Writer = os.Stdout
 )
 
 func duckhouseNewConnID(c net.Conn) uint64 {
@@ -202,10 +206,60 @@ func duckhouseHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Not Found\r\n")
 }
 
+type wrapResponseWriter struct {
+	base   http.ResponseWriter
+	status int
+	bsize  int
+}
+
+func (w *wrapResponseWriter) Header() http.Header {
+	return w.base.Header()
+}
+
+func (w *wrapResponseWriter) Write(data []byte) (int, error) {
+	n, err := w.base.Write(data)
+	w.bsize += n
+	return n, err
+}
+
+func (w *wrapResponseWriter) WriteHeader(statusCode int) {
+	w.base.WriteHeader(statusCode)
+	w.status = statusCode
+}
+
+func recordCombinedAccessLog(w io.Writer, r *http.Request, status, bodySize int) {
+	remoteAddr := r.RemoteAddr
+	ident := "-"
+	user := "-"
+	if r.URL.User != nil {
+		user = r.URL.User.Username()
+	}
+	timestamp := time.Now().Format("02/Jan/2006:15:04:05 -0700")
+	requestLine := fmt.Sprintf("%s %s %s", r.Method, r.URL.RequestURI(), r.Proto)
+	referer := r.Referer()
+	if referer == "" {
+		referer = "-"
+	}
+	userAgent := r.UserAgent()
+	if userAgent == "" {
+		userAgent = "-"
+	}
+	fmt.Fprintf(w, "%s %s %s [%s] \"%s\" %d %d \"%s\" \"%s\"\n",
+		remoteAddr, ident, user, timestamp, requestLine, status, bodySize, referer, userAgent)
+}
+
+func middlewareCombinedAccessLogger(logWriter io.Writer, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		ww := &wrapResponseWriter{base: w}
+		h.ServeHTTP(ww, r)
+		recordCombinedAccessLog(logWriter, r, ww.status, ww.bsize)
+	})
+}
+
 func run2() error {
 	srv := &http.Server{
 		Addr:        "localhost:9998",
-		Handler:     http.HandlerFunc(duckhouseHandler),
+		Handler:     middlewareCombinedAccessLogger(accessLogWriter, http.HandlerFunc(duckhouseHandler)),
 		ConnContext: duckhouseConnContext,
 		ConnState:   duckhouseConnState,
 	}
