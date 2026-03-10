c9c9f8404ff2cfddaf291bd8199de55f258c6113
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-03-10 23:21:15 +0900

    "html" formatter - table in HTML

---
+++ internal/formatter/html/html.go
@@ -0,0 +1,56 @@
+package table
+
+import (
+	"database/sql"
+	"io"
+
+	"github.com/koron/duckhouse/internal/formatter"
+	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/renderer"
+)
+
+func init() {
+	formatter.Register(&Factory{}, "html")
+}
+
+type Factory struct {
+}
+
+var _ formatter.Factory = (*Factory)(nil)
+
+func (f *Factory) ContentType() string {
+	return "text/html"
+}
+
+func (f *Factory) Create(w io.Writer, params map[string]string) (formatter.Writer, error) {
+	var opts []tablewriter.Option
+	opts = append(opts, tablewriter.WithRenderer(renderer.NewHTML()))
+	// FIXME: Apply params
+	table := tablewriter.NewTable(w, opts...)
+	return &Writer{
+		t: table,
+	}, nil
+}
+
+type Writer struct {
+	t *tablewriter.Table
+}
+
+var _ formatter.Writer = (*Writer)(nil)
+
+func (w *Writer) WriteHeader(columnTypes []*sql.ColumnType) error {
+	elements := make([]string, len(columnTypes))
+	for i, typ := range columnTypes {
+		elements[i] = typ.Name()
+	}
+	w.t.Header(elements)
+	return nil
+}
+
+func (w *Writer) WriteBody(values []any) error {
+	return w.t.Append(values)
+}
+
+func (w *Writer) Flush() error {
+	return w.t.Render()
+}
--- plugins.go
+++ plugins.go
@@ -2,5 +2,6 @@ package main
 
 import (
 	_ "github.com/koron/duckhouse/internal/formatter/csv"
+	_ "github.com/koron/duckhouse/internal/formatter/html"
 	_ "github.com/koron/duckhouse/internal/formatter/table"
 )
