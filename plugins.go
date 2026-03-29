e0f4caa59edf78a4cb0c38b1eca23adc87bc9f35
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-03-29 09:51:24 +0900

    Switch to duckserver

--- plugins.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	_ "github.com/koron/duckhouse/internal/formatter/avro"
-	_ "github.com/koron/duckhouse/internal/formatter/csv"
-	_ "github.com/koron/duckhouse/internal/formatter/html"
-	_ "github.com/koron/duckhouse/internal/formatter/markdown"
-	_ "github.com/koron/duckhouse/internal/formatter/table"
-)
